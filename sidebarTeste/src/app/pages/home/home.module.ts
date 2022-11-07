0262f06bde47428e047b74a0abc7a8cd92d6a339
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2022-11-07 16:00:21 -0300

    teste concluido

--- sidebarTeste/src/app/pages/home/home.module.ts
+++ sidebarTeste/src/app/pages/home/home.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import { DashboardModule } from './pages/dashboard/dashboard.module';
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentsModule } from './components/components.module';
-import { UserModule } from './pages/user/user.module';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent],
+  declarations: [HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,12 +25,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    DashboardModule,
     ComponentsModule,
-    DashboardModule,
-    UserModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class HomeModule {}
