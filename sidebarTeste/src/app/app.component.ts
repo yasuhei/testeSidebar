f8f875ed5defcd281121d269b1f7d5eab54d6bae
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2022-11-07 11:58:43 -0300

    sidebar desktop

---
+++ sidebarTeste/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) {}
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((response) => {
+      if (response.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+}
